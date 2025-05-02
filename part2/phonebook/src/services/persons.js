24ed2fae54cfbe5ee4e8d8ad75fac9e3f96d1722
N-Steph <snouaffo@gmail.com>
2025-05-02 08:52:49 +0100

    module communicating with json-server

---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3002/persons'
+
+const getAll = () => axios.get(baseUrl)
+
+const postPerson = (newPerson) => axios.post(baseUrl, newPerson)
+
+const deletePerson = (id) => axios.delete(`${baseUrl}/${id}`)
+
+const update = (person) => axios.put(`${baseUrl}/${person.id}`, person)
+
+export default {getAll, postPerson, deletePerson, update}
