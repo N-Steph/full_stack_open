26c2f0bb928ba4389c990a5af1d5dd94c010ae43
N-Steph <snouaffo@gmail.com>
2025-05-05 19:39:04 +0100

    allowd CORS

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 persons = [
@@ -28,6 +29,7 @@ persons = [
 morgan.token('data', function getPostData(req) {
     return JSON.stringify(req.body)
 })
+app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
