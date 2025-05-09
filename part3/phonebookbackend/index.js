2cd0fb8e10502b4e66f4f5b54503d3f4b3ceb14c
N-Steph <snouaffo@gmail.com>
2025-05-09 06:08:40 +0100

    updated the routes to work with the database

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -43,49 +43,78 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const numberOfPerson = persons.length
-    response.send(`
-        <p>Phonebook has info of ${numberOfPerson} people<p>
-        <p>${Date()}</p>
-        `)
+    Person.find({}).then(result => {
+        const numberOfPerson = persons.length
+        return response.send(`
+            <p>Phonebook has info of ${numberOfPerson} people<p>
+            <p>${Date()}</p>
+            `)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    const personFound = persons.filter(person => person.id === id)
-    if (personFound.length === 0) {
-        return response.status(404).end()
-    }
-    response.json(personFound)
+    Person.findById(id)
+        .then(result => {
+            if (result) {
+                return response.json(result)
+            }
+            else {
+                return response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
+    Person.findByIdAndDelete(id).then(result => {
+        response.status(204).end
+    }).catch(error => next(error))
+
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
 })
 
 app.post('/api/persons', (request, response) => {
     const newPerson = request.body
-    if (!newPerson.name || newPerson.name.length === 0 || !newPerson.number || newPerson.number.length === 0) {
-        return response.status(400).json({ error: 'name and number must be stated' })
-    }
-    // const result = persons.map(person => person.name.toLowerCase()).filter(name => name === newPerson.name.toLowerCase())
-    // if (result.length !== 0) {
-    //     return response.status(400).json({ error: 'name must be unique' })
-    // }
-    // const id = Math.floor((Math.random() * 2000))
-    // persons = persons.concat({ id: String(id), ...newPerson })
     const name = newPerson.name
     const number = newPerson.number
-    const person = new Person({
-        name: name,
-        number: number
+    if (!name || name.length === 0 || !number || number.length === 0) {
+        return response.status(400).json({ error: 'name and number must be stated' })
+    }
+    Person.find({ name: name }).then(result => {
+        if (result) {
+            Person.findOneAndUpdate({ name: name }, { $set: { name: name, number: number } })
+                .then(result => {
+                    response.status(204).end()
+                })
+        }
+        else {
+            const person = new Person({
+                name: name,
+                number: number
+            })
+            return person.save().then(result => {
+                response.json(result)
+            })
+        }
     })
-    person.save().then(result => {
-        response.json(result)
-    })
+
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
