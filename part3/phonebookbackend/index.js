b8378dc7ec659d30bb137530a299df51786f20a8
N-Steph <snouaffo@gmail.com>
2025-05-10 13:35:06 +0100

    implemented data validation

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -5,33 +5,11 @@ const cors = require('cors')
 const Person = require('./models/person')
 const app = express()
 
-persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    // {
-    //     "id": "4",
-    //     "name": "Mary Poppendieck",
-    //     "number": "39-23-6423122"
-    // }
-]
-
+app.use(cors())
 morgan.token('data', function getPostData(req) {
     return JSON.stringify(req.body)
 })
-app.use(cors())
+
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
@@ -44,7 +22,7 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
     Person.find({}).then(result => {
-        const numberOfPerson = persons.length
+        const numberOfPerson = result.length
         return response.send(`
             <p>Phonebook has info of ${numberOfPerson} people<p>
             <p>${Date()}</p>
@@ -69,38 +47,34 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     Person.findByIdAndDelete(id).then(result => {
-        response.status(204).end
+        response.status(204).end()
     }).catch(error => next(error))
-
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
 })
 
-app.post('/api/persons', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+    const person = request.body
+    console.log(id)
+    Person.findOneAndUpdate({ _id: id }, { $set: { number: person.number } }, { runValidators: true })
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
     const newPerson = request.body
     const name = newPerson.name
     const number = newPerson.number
-    if (!name || name.length === 0 || !number || number.length === 0) {
-        return response.status(400).json({ error: 'name and number must be stated' })
-    }
-    Person.find({ name: name }).then(result => {
-        if (result) {
-            Person.findOneAndUpdate({ name: name }, { $set: { name: name, number: number } })
-                .then(result => {
-                    response.status(204).end()
-                })
-        }
-        else {
-            const person = new Person({
-                name: name,
-                number: number
-            })
-            return person.save().then(result => {
-                response.json(result)
-            })
-        }
+    const person = new Person({
+        name: name,
+        number: number
     })
-
+    person.save()
+        .then(result => {
+            response.json(result)
+        })
+        .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -108,6 +82,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
