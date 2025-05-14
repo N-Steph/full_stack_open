4c732298784f9719f6dca3293b92a28506bf20a3
N-Steph <snouaffo@gmail.com>
2025-05-14 12:16:46 +0100

    defined the log structure of server using morgan library and enabled cross origin resource sharing

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,13 +2,19 @@ const config = require('./utils/config')
 const express = require('express')
 const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 
 const app = express()
 
 mongoose.connect(config.MONGODB_URI).then(response => console.log('connected to mongodb'))
 
+morgan.token('data', function getPostData(req) {
+    return JSON.stringify(req.body)
+})
 
+app.use(cors())
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use('/api', blogRouter)
 
 module.exports = app
