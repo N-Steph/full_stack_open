d4859fa7714f98dd1ca09b12df1df91380fe09d9
N-Steph <snouaffo@gmail.com>
2025-05-14 13:32:06 +0100

    updated the post request to default the likes property to 0 when absent in blog

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,18 +1,31 @@
 const Blog = require('../models/blog')
 const blogRouter = require('express').Router() 
 
-blogRouter.get('/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
+blogRouter.get('/blogs', async (request, response) => {
+  try {
+    const blogs = await Blog.find({})
     response.json(blogs)
-  })
+  }
+  catch(error) {
+    console.log(error)
+  }
+  
 })
 
-blogRouter.post('/blogs', (request, response) => {
+blogRouter.post('/blogs', async (request, response) => {
   const blog = new Blog(request.body)
+  if (!blog.likes) {
+    blog.likes = 0
+  }
 
-  blog.save().then((result) => {
+  try {
+    const result = await blog.save()
     response.status(201).json(result)
-  })
+  }
+  catch(error) {
+    console.log(error)
+  }
+  
 })
 
 module.exports = blogRouter
