d0668c72af55d5bf38618731fb50794044b593e0
N-Steph <snouaffo@gmail.com>
2025-05-14 12:24:14 +0100

    testing routes

---
+++ part4/bloglist/tests/blogs_api.test.js
@@ -0,0 +1,70 @@
+const app = require('../app')
+const { test, beforeEach, after } = require('node:test')
+const assert = require('node:assert')
+const Blog = require('../models/blog')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const api = supertest(app)
+
+const blogs = [
+    {
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+    },
+    {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+    },
+    {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    await Blog.insertMany(blogs)
+})
+
+test("blogs are return as json", async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+
+  assert.strictEqual(response.body.length, blogs.length)
+})
+
+test("post new data correctly", async () => {
+    const newBlog = {
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10
+      }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const blogsNewState = await Blog.find({})
+    assert.equal(blogsNewState.length, blogs.length + 1)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
