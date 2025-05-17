246794802449ff264f1bb27d654c66ad0e66be00
N-Steph <snouaffo@gmail.com>
2025-05-17 12:48:34 +0100

    set the format of object return by get request

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -9,6 +9,13 @@ const userSchema = mongoose.Schema({
     passwordHash: String
 })
 
+userSchema.set('toJson', (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+})
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
