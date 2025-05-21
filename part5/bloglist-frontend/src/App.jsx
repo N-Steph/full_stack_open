0ff1ac4d64b6c4ebf81d6907c5741170e882d7f5
N-Steph <snouaffo@gmail.com>
2025-05-21 14:48:15 +0100

    initial commit

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form>
+          username <input />
+          password <input />
+          <button>login</button>
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+
+export default App
