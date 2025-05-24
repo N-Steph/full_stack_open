6606f73703c7d1135191825c79bce3222426cfc8
N-Steph <snouaffo@gmail.com>
2025-05-24 09:41:21 +0100

    Add button to each blog which controls whether all of the details about the blog are shown or not

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -16,9 +16,9 @@ const App = () => {
   const newBlogRef = useRef()
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then(blogs => {
+      setBlogs(blogs)
+    })
   }, [])
 
   const loginHandler = async (event) => {
--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+
+    <div style={blogStyle}>
+      <div style={{display: visible ? 'none' : ''}}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={{display: visible ? '' : 'none'}}>
+        {blog.title} {blog.author}
+        <p>{blog.url}</p>
+        <p>likes {blog.likes} <button onClick={toggleVisibility}>hide</button></p>
+        <p>{blog.user.username}</p>
+      </div>
+  </div>
+)}
 
 export default Blog
